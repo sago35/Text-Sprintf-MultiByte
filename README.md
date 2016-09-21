45a53d6c24971de55ed04cbbd5c53ac8077afcb9
sago35 <sago35@gmail.com>
2016-09-21 19:31:59 +0900

    Initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Text::Sprintf::MultiByte - It's new $module
+
+# SYNOPSIS
+
+    use Text::Sprintf::MultiByte;
+
+# DESCRIPTION
+
+Text::Sprintf::MultiByte is ...
+
+# LICENSE
+
+Copyright (C) sago35.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+sago35 <sago35@gmail.com>
---
+++ lib/Text/Sprintf/MultiByte.pm
@@ -0,0 +1,39 @@
+package Text::Sprintf::MultiByte;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Text::Sprintf::MultiByte - It's new $module
+
+=head1 SYNOPSIS
+
+    use Text::Sprintf::MultiByte;
+
+=head1 DESCRIPTION
+
+Text::Sprintf::MultiByte is ...
+
+=head1 LICENSE
+
+Copyright (C) sago35.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+sago35 E<lt>sago35@gmail.comE<gt>
+
+=cut
+
